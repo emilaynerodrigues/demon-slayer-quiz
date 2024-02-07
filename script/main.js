515b3f22d0cbed489d7a22df182c9e68b4345f02
Emilayne Rodrigues <emilaynx@gmail.com>
2024-02-06 21:17:12 -0400

    finalizando toda a parte de javascript do projeto

--- script/main.js
+++ script/main.js
@@ -25,7 +25,7 @@ const perguntas = [
   // pergunta 3
   {
     pergunta: "Quem é o Hashira das Serpentes em Demon Slayer?",
-    respostas: ["Mitsuri Kanroji", "Muichiro Tokito", "Tengen Uzui"],
+    respostas: ["Mitsuri Kanroji", "Muichiro Tokito", "Obanai Iguro"],
     correta: 2,
   },
   // pergunta 4
@@ -74,18 +74,45 @@ const perguntas = [
 const quiz = document.querySelector("#quiz");
 const template = document.querySelector("template");
 
+const corretas = new Set(); //irá guardar a quantidade de repostas corretas
+const totalDePerguntas = perguntas.length; //verificando tamanho do array de perguntas, ou seja, quantos item existem no array
+const mostrarTotal = document.querySelector("#acertos span"); //selecionando o span
+//iniciando mudança de dados do span mostrarTotal de acordo com numero de repostas corretas
+mostrarTotal.textContent = corretas.size + " de " + totalDePerguntas; 
+
 // laço de repetição para criar o quiz dinamicamente a partir do js
 for (const item of perguntas) {
   // para cada item de perguntas
   const quizItem = template.content.cloneNode(true); // irá clonar cada item (filhos) do nó principal (<template>) --> copia
   quizItem.querySelector("h3").textContent = item.pergunta; // mostrando cada pergunta na tela
 
-  for (let reposta of item.respostas) {
+  for (let resposta of item.respostas) {
     // atribuindo a resposta cada item do sub array de respostas
     const dt = quizItem.querySelector("dl dt").cloneNode(true); // clonando os nós de <dt>
-    dt.querySelector("span").textContent = reposta;
+    dt.querySelector("span").textContent = resposta;
+    dt.querySelector("input").setAttribute(
+      "name",
+      "pergunta-" + perguntas.indexOf(item)
+    ); //vai atribuir o valor da proprodade "name" dentro do html se acordo com o index de cada pergunta, para facilitar a busca pela reposta
+    dt.querySelector("input").value = item.respostas.indexOf(resposta); //vai setar a valor atribuido a cada reposta de acordo como seu index;
 
-    quizItem.querySelector("dl").appendChild(dt); // mostrando cada resposta da pergunta --> mostrando cada reposta do array[item]
+    //verificando se a reposta é a correta
+    // --> verificando se houve alguma mudança nos inputs quando o usuario selecionar algum input de reposta
+    dt.querySelector("input").onchange = (event) => {
+      //verifica se a reposta selecionada está correta, comparando os valores da resposta selecionada com o reposta correta do array de perguntas (item of respostas) atraves de seus indexes
+      const estaCorreta = event.target.value == item.correta;
+
+      //caso usuario clique em uma reposta errada, será subtraido do resultado, ou seja, se o usuario responder a resposta correta e mudar de opção, será retirado o item do set() corretas
+      corretas.delete(item);
+      //se a reposta estiver correta, o programa seguirá
+      if (estaCorreta) {
+        //se usuario selecionar resposta correta será adicionado a contagem de repostas corretas -- o item será adicionado ao Set()
+        corretas.add(item);
+      }
+
+      mostrarTotal.textContent = corretas.size + " de " + totalDePerguntas; //mudando dados do span mostrarTotal de acordo com numero de repostas corretas apos usuario selecionar repostas
+    };
+    quizItem.querySelector("dl").appendChild(dt); // mostrando cada resposta da pergunta --> mostrando cada reposta do array[item] -- adicionando filhos
   }
   quizItem.querySelector("dl dt").remove(); // removendo primeiro item do quiz --> modelo de resposta usado ('Resposta A')
 
